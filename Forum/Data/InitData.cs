a6e80d9f441659e0c5188c28f9b956c86f1b9f27
Gryniam <muzikaeng@gmail.com>
2022-08-15 14:14:04 +0300

    Init data in data base

---
+++ Forum/Data/InitData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Data.Interfaces;
+using Forum.Data.EFramework;
+using Forum.Models;
+
+namespace Forum.Data
+{
+    public class InitData
+    {
+        public static void Initialize(AppDBContent context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.AddRange(
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Login = "Gryniam",
+                        Mail = "muzikaeng@gmail.com",
+                        Password = "787898p",
+                        RoleId = 0,
+                        AdditionalInf = "Put in hu ilo",
+                        IsBanned = false,
+                        UserRate = 0f,
+                        CreatedTopics = 0,
+                        AnswersTopics = 0
+
+                    }
+                    ) ;
+            }
+        }
+    }
+}
